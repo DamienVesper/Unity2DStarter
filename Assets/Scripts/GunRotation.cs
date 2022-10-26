9675fc3240772cff763d042e48dd61d139316751
Damien Vesper <ldamienvesper@gmail.com>
2022-10-26 00:35:23 -0500

    add a gun, and rotate it to cursor

---
+++ Assets/Scripts/GunRotation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotation : MonoBehaviour {
+    private Vector2 mousePos; // A vector quantity containing current mouse position on the screen.
+    private Vector3 offset = new Vector3(0.875f, 0, 0); // The offset of the gun relative to the player.
+
+    public Camera camera; // The camera attached to the scene.
+    public Rigidbody2D rb; // The Rigidbody of the player.
+
+    void Start () {
+        // Set references.
+        camera = Camera.main;
+    }
+
+    void Update () {
+        // Set references.
+        mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate () {
+        Vector3 playerPos = GameObject.Find("Player").transform.position;
+
+        // The point on the scene the gun should face.
+        Vector2 focusPoint = mousePos - new Vector2(playerPos.x, playerPos.y);
+
+        // Calculate the angle of a line that passes through the focus point.
+        // From unit circle knowledge this is just a linear function with slope equivalent to tangent of the angle.
+        // Using this we can use the magnitude of the vector as a line between the gun and cursor position, and it's slope will be tangent of the angle.
+        // Taking the inverse gives the angle that the gun should point at, in radians. This is then converted to degrees.
+        float gunDirection = Mathf.Atan2(focusPoint.y, focusPoint.x) * Mathf.Rad2Deg;
+        rb.rotation = gunDirection; // Set the rotation of the gun to determined angle.
+
+        // Move the gun to player position with certain offset.
+        // The offset is calculated by rotating the offset vector by gunDirection along the z-axis.
+        rb.MovePosition(playerPos + (Quaternion.Euler(0, 0, rb.rotation) * offset));
+    }
+}
